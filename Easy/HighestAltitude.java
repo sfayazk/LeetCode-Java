15b146f2827f6a76a7329096ed885d0fd7112d83
sfayazk <thefayazk@gmail.com>
2026-07-28 22:52:39 +0530

    Solve LeetCode 1732: Find the Highest Altitude

---
+++ Easy/HighestAltitude.java
@@ -0,0 +1,13 @@
+class HighestAltitude {
+    public int largestAltitude(int[] gain) {
+        int highestAltitude = 0;
+        int currentAltitude = 0;
+        for(int i=0; i<gain.length; i++){
+            currentAltitude += gain[i];
+            if(currentAltitude>highestAltitude){
+                highestAltitude = currentAltitude;
+            }
+        }
+        return highestAltitude;
+    }
+}
