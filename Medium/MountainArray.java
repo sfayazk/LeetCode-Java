60447bbb367515e8bcfac2049361e4cf8b2858d7
sfayazk <thefayazk@gmail.com>
2026-08-03 21:50:07 +0530

    Update: Optimized solution - LeetCode 852

--- Medium/MountainArray.java
+++ Medium/MountainArray.java
@@ -1,4 +1,5 @@
 // Brute Force Approach
+
 class MountainArray {
     public int peakIndexInMountainArray(int[] arr) {
         int index = 0;
@@ -8,5 +9,24 @@ class MountainArray {
             i++;
         }
         return index;
+    }
+}
+
+// Optimized solution
+
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length-1;
+        while(start<end){
+            int mid = start + (end-start) / 2;
+            if(arr[mid]>arr[mid+1]){
+                end = mid;
+            }
+            else if (arr[mid]<arr[mid+1]){
+                start = mid + 1;
+            }
+        }
+            return start;
     }
 }
